5fd0fa02017823a4643e9637db18111821abf7ed
Reem <s202360310@kfupm.edu.sa>
2026-03-30 13:18:29 +0300

    TODOS8-9

--- 6-1-back-end-fundamentals/src/App.jsx
+++ 6-1-back-end-fundamentals/src/App.jsx
@@ -369,6 +369,15 @@ export default function App() {
   //   .then((data) => {
   //     setStudent(data);
   //   });
+    const [student, setStudent] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/student')
+      .then((res) => res.json())
+      .then((data) => {
+        setStudent(data);
+      });
+  }, []);
 
   return (
     <main className="app-shell">
